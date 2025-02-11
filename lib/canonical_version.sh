c1e96007f50d74c3002a3f1c265fd91bcf96f64e
Chad Fennell <chadfennell@users.noreply.github.com>
2025-02-11 09:57:38 -0700

    Debugging: show available erlang versions

--- lib/canonical_version.sh
+++ lib/canonical_version.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+output_section() {
+  local title="$1"
+  echo "-----> $title"
+}
+
 erlang_builds_url() {
   case "${STACK}" in
     "heroku-20")
@@ -15,23 +20,24 @@ erlang_builds_url() {
   echo $erlang_builds_url
 }
 
-fetch_elixir_versions() {
-  url="https://builds.hex.pm/builds/elixir/builds.txt"
-  curl -s "$url" | awk '/^v[0-9.]+[- ]/ { print $1 }'
-}
-
 fetch_erlang_versions() {
+  output_section "Stack selected: ${STACK:-default}"
+  output_section "Fetching available Erlang versions..."
+  
   case "${STACK}" in
     "heroku-20")
       url="https://builds.hex.pm/builds/otp/ubuntu-20.04/builds.txt"
+      output_section "Using Ubuntu 20.04 Erlang builds"
       curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
       ;;
     "heroku-22")
       url="https://builds.hex.pm/builds/otp/ubuntu-22.04/builds.txt"
+      output_section "Using Ubuntu 22.04 Erlang builds"
       curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
       ;;
     *)
       url="https://raw.githubusercontent.com/HashNuke/heroku-buildpack-elixir-otp-builds/master/otp-versions"
+      output_section "Using HashNuke's cedar-14 Erlang builds"
       curl -s "$url"
       ;;
   esac
@@ -64,11 +70,20 @@ exact_elixir_version_available() {
 
 check_erlang_version() {
   version=$1
+  output_section "Checking Erlang version: $version"
+  
+  echo "Available versions:"
+  fetch_erlang_versions | while read -r line; do
+    echo "       $line"
+  done
+  
   exists=$(exact_erlang_version_available "$version" "$(fetch_erlang_versions)")
   if [ $exists -ne 0 ]; then
     output_line "Sorry, Erlang '$version' isn't supported yet or isn't formatted correctly. For a list of supported versions, please see https://github.com/HashNuke/heroku-buildpack-elixir#version-support"
     exit 1
   fi
+  
+  output_section "Erlang version $version is available"
 }
 
 check_elixir_version() {
